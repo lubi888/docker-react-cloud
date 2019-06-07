9b6a80d9b2e4a0b421d813ec1a365e6d03ae4e27
lubi888 <linuxubiquitous@gmail.com>
2019-06-07 12:06:43 +0100

    init react docker app

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          another para updated bank holiday monday , docker fedora
+        </p>
+	       <p>This is also live now just like its a rainy day</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
